8901338ac40e641d4d1a6ec16ecb5705bae91410
M Anwari L <lm.anwari@gmail.com>
2023-03-11 18:28:10 +0700

    added db config.

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from db_config import db_user, db_password, db_host, db_name
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@winhost/db_fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{dn_host}/{db_name}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
